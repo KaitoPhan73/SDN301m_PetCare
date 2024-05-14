38e619b553868f379d1cc2be0e276691636b10aa
khai732003 <khaithinhvui2003@gmail.com>
2024-05-14 15:40:36 +0700

    khai-update

--- Back_End/src/routes/api.ts
+++ Back_End/src/routes/api.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response, Router } from "express";
-import { getUsers, getUser } from "../controllers/UserController";
+import { getUsers, getUser, insertUser } from "../controllers/UserController";
 
 const router: Router = express.Router();
 
 router.get("/users", getUsers);
 
-router.get("/user/:userId", (req: Request, res: Response) => {
-  getUser(req, res);
-});
+router.get("/user/:userId", getUser);
+
+router.post("/user", insertUser);
 
 export default router;
